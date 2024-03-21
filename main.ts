0bf0df313c7a97bfe3702860b7576c475587af31
SidraRaza <160983959+SidraRaza@users.noreply.github.com>
2024-03-21 22:22:14 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+
+import inquirer from "inquirer";
+const answers = await inquirer.prompt([{
+    name:"numberone",
+    type:"number",
+    message:"Kindly enter your first no:"
+
+},
+{
+    name:"numbertwo",
+    type:"number",
+    message:"Kindly enter your first no:"
+
+},
+{
+    name:"operator",
+    type:"list",
+    choices:["*","/","-","+","%"],
+    message:"Select operator:"
+
+},
+
+]);
+const{numberone,numbertwo,operator}=answers;
+if(numberone && numbertwo && operator){
+    let result:number=0;
+    if(operator === "+"){
+        result = numberone+numbertwo
+    }
+    else if(operator === "-"){
+        result = numberone-numbertwo
+    }
+    else if(operator === "*"){
+        result = numberone*numbertwo
+    }
+    else if(operator === "/"){
+        result = numberone/numbertwo
+    }
+    else if(operator === "%"){
+        result = numberone%numbertwo
+    }
+    console.log("Your result is:",result);
+}
+else{
+    console.log("Kindly enter your valid input");
+}
